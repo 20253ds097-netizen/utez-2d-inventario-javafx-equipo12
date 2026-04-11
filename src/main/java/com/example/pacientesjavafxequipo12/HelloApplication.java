7fd277eaeab62ed762c7d6af63acfdee3c4859dd
Luis Uriel Vargas Espino <uriel98@gmail.com>
2026-04-10 20:06:55 -0600

    Se agrega la parte del View sus controllers y la vista

--- src/main/java/com/example/pacientesjavafxequipo12/HelloApplication.java
+++ src/main/java/com/example/pacientesjavafxequipo12/HelloApplication.java
@@ -4,16 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(
+            "/com/example/pacientesjavafxequipo12/MainView.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Gestión de Pacientes");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
