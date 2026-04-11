639decf89b82d49f4d88afd1e5696c3ab1b07291
Alan Esteban <AlanEsteban89@gmail.com>
2026-04-10 21:16:00 -0600

    update

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pacientesjavafxequipo12 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pacientesjavafxequipo12 to javafx.fxml;
-    exports com.example.pacientesjavafxequipo12;
-}
